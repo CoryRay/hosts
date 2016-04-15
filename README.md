7f902a9da20e5298353d5f05ed0007ce3e0bd625
Cory Rodriguez <coreyreylp@gmail.com>
2016-04-15 15:40:01 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Hosts Adblocker Thing
+
+I initally found it somewhere online, and edited it slightly
--- hostsUpdate.sh
+++ hostsUpdate.sh
@@ -9,7 +9,6 @@ set -o errexit
 ##### Modified from: https://github.com/fr0stycl34r/update-hosts
 ###############################################################################
 
-
 ## We must be root in order to modify the contents of /etc
 rootcheck() {
   if [[ $UID -ne 0 ]]; then
@@ -19,14 +18,11 @@ rootcheck() {
 }
 rootcheck
 
-
 ## Backup the current hosts file
 mv /etc/hosts /etc/hosts.bak
 
-
 ## Create a temporary file to dump the various lists into
 temphosts=$(mktemp)
-
 
 ## Download various pre-made lists into our temp file
 wget -nv -O - http://someonewhocares.org/hosts/hosts >> $temphosts
@@ -35,19 +31,15 @@ wget -nv -O - http://www.malwaredomainlist.com/hostslist/hosts.txt >> $temphosts
 wget -nv -O - http://adblock.gjtech.net/?format=hostfile >> $temphosts
 wget -nv -O - http://hosts-file.net/ad_servers.asp >> $temphosts
 wget -nv -O - "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" >> $temphosts
-
 wget -nv -O - http://adaway.org/hosts.txt >> $temphosts
-
 
 ## Cleanup the list; remove commented lines, remove duplicates, etc
 sed -e '/^127.0.0.1/!d' -e '/localhost/d' -e 's/0.0.0.0/127.0.0.1/' -e 's/ \+/\t/' -e 's/#.*$//' -e 's/[ \t]*$//' < $temphosts | sort -u > /etc/hosts
-
 
 ## Append some necessary stuff to the beginning of the file
 echo "# Last updated on $(date)
 127.0.0.1 localhost
 127.0.0.1 $HOSTNAME" | cat - /etc/hosts >> temp && mv temp /etc/hosts
-
 
 ## A bit of cleanup
 rm $temphosts
